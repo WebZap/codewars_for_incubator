771adc84281b6de32dd6abce3a462660acceaa34
Pavel <zap.official@mail.ru>
2023-03-22 00:20:05 +0300

    second commit

---
+++ How_good_are_you_really.js
@@ -0,0 +1,46 @@
+"use strict"
+//classPoints - array with points your friend
+//yourPoints - it only your point
+//let's get started
+function betterThanAverage(classPoints, yourPoints) {
+    let pointsFriend = classPoints;
+    let onlyYourPoint = yourPoints;
+    let pointsFriendSum = 0
+    let result = false;
+    
+    pointsFriend.forEach((element) => {
+        pointsFriendSum += element;
+    })
+
+    let middleFriendPoint = pointsFriendSum / pointsFriend.length;
+    if ( middleFriendPoint > onlyYourPoint){
+        result = false;
+    } else {
+        result = true;
+    }
+
+    return result;
+  }
+  
+ console.log( betterThanAverage([12, 23, 34, 45, 56, 67, 78, 89, 90],9) );
+
+ function findNeedle(haystack) {
+    // your code here
+    let needleItem = 'needle';
+    let index = haystack.indexOf('needle')
+    let result = null;
+    haystack.forEach((element)=>{
+      if (element == needleItem){
+        result = 'found the needle at position '+ (index);
+      } else {
+        return "Your function didn't return anything"
+      }
+    })
+    return result;
+  }
+  
+  
+  console.log(findNeedle(['3', '123124234', undefined, 'needle', 'world', 'hay', 2, '3', true, false]))
+
+  let choto = [1,2,3,4]
+  console.log(choto[1])
