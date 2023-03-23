9c513b1d7395731e1b1ae8a1f6b5fdddbd24c710
Pavel <zap.official@mail.ru>
2023-03-23 15:37:22 +0300

    justsave

--- How_good_are_you_really.js
+++ How_good_are_you_really.js
@@ -6,34 +6,9 @@
 
 //---------------------------------------------------------------------------------------------------------
 
- function findNeedle(haystack) {
-    // your code here
-    let needleItem = 'needle';
-    let index = haystack.indexOf('needle')
-    let result = null;
-    haystack.forEach((element)=>{
-      if (element == needleItem){
-        result = 'found the needle at position '+ (index);
-      } else {
-        return "Your function didn't return anything"
-      }
-    })
-    return result;
-  }
-  console.log(findNeedle(['3', '123124234', undefined, 'needle', 'world', 'hay', 2, '3', true, false]))
-
+ 
 //---------------------------------------------------------------------------------------------------------
 
- function validatePIN (pin) {
-    let countOfPin = Array.from(String(pin)).length
-    let checkSymb = /^\d+$/.test(pin);
-    if ((countOfPin == 4 || countOfPin == 6) && checkSymb == true) {
-      return true;
-    } else {
-      return false;
-    } 
-  }
-  console.log(validatePIN('a234'));
 
 //---------------------------------------------------------------------------------------------------------
 
